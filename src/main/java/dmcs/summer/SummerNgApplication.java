8c501ac371042b845da598f0d4a5d6dcfbb2e62e
howorang <howorang@gmail.com>
2019-04-22 19:01:16 +0200

    Initial commit

---
+++ src/main/java/dmcs/summer/SummerNgApplication.java
@@ -0,0 +1,13 @@
+package dmcs.summer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerNgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerNgApplication.class, args);
+    }
+
+}
