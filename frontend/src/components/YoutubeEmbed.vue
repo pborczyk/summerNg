cc27d1b8da76468e285ea925a93b19c3c5711fa8
howorang <howorang@gmail.com>
2019-05-19 22:25:07 +0200

    embed youtube player

---
+++ frontend/src/components/YoutubeEmbed.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <iframe id="ytplayer" type="text/html" width="640" height="360"
+                v-bind:src="embedSrc"
+                frameborder="0">
+        </iframe>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import {environment} from "@/env/DevEnv";
+    @Component
+    export default class Entry extends Vue {
+        @Prop() private videoId!: string;
+        @Prop() private autoplay!: boolean;
+
+        get embedSrc(): string {
+            return 'http://www.youtube.com/embed/' + this.videoId + '?autoplay=' + this.autoplay ? '1' : '0'
+                + '&origin=' + environment.appUrl;
+        }
+    }
+
+</script>
+
+<style scoped>
+    .yt-player {
+        width: 640px;
+        height: 360px;
+    }
+</style>
